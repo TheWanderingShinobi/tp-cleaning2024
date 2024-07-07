9cf60624d0a90c074f4b8bcf415465a0476554c9
TheWanderingShinobi <thewanderingshinobi@proton.me>
2024-07-07 01:04:15 -0400

    New Nav

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const hamburger = document.querySelector("#hamburger");
-const menu = document.querySelector("#menu");
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
 
-hamburger.addEventListener("click", () => {
-	menu.classList.toggle("hidden");
+hamburger?.addEventListener('click', () => {
+  menu?.classList.toggle('hidden');
 });
